386acba4f5351d60c48f3155129de2b8f99a7df0
kunihikot <inverse.of.void.matrix@gmail.com>
2015-05-27 11:24:55 +0900

    added missing imports

--- util.go
+++ util.go
@@ -1,6 +1,9 @@
 package valval
 
-import "reflect"
+import (
+	"gopkg.in/guregu/null.v2"
+	"reflect"
+)
 
 func NewStringValidator(inner func(string) error) ValidatorFunc {
 	return func(val interface{}) error {
